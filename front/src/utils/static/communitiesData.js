0b8f3f39f7491268ceffc1a2257d8835a3e72626
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-03-10 07:26:54 +0530

    fix: communities page image loading issue

--- front/src/utils/static/communitiesData.js
+++ front/src/utils/static/communitiesData.js
@@ -1,45 +1,43 @@
 export const communitiesData = [
-    {
-        title: "Dubai Communities",
-        cards: [
-            {
-                id : 1,
-                name: "Jumeirah Village Circle Area Guide",
-                desc: "One of the master-planned communities for families",
-                img:"../../../public/assets/img/communitiesCard-1.svg"
-            },
-            {
-                id : 2,
-                name: "Palm Jumeirah",
-                desc:"Ideal island backed with beautiful beaches overlooking stunning views of Skyline.",
-                img:"../../../public/assets/img/communitiesCard-2.svg"
-            },
-            {
-                id : 3,
-                name: "Dubai Hill Estate",
-                desc: "The city’s hub, Dubai Marina is a meticulously planned community.",
-                img:"../../../public/assets/img/communitiesCard-3.svg"
-            },
-            {
-                id : 4,
-                name: "Dubai Marina",
-                desc : "A lush 2700 acers suburban lifestyle with  lush greenery. ",           
-                img:"../../../public/assets/img/communitiesCard-4.svg"
-            },
-            {
-                id : 5,
-                name: "Dubai Square",
-                desc: "Town square is divided into 13 sub-projects spread among 1.6 million square feet of land.",
-                img:"../../../public/assets/img/communitiesCard-5.svg"
-            },
-            {
-                id : 6,
-                name: "Down Town Dubai",
-                desc: "Downtown Dubai, as the name implies, is the heart of Dubai, constantly evolving.",
-                img:"../../../public/assets/img/communitiesCard-6.svg"
-            }
-
-        ]
-    }
+  {
+    title: "Dubai Communities",
+    cards: [
+      {
+        id: 1,
+        name: "Jumeirah Village Circle Area Guide",
+        desc: "One of the master-planned communities for families",
+        img: "/assets/img/communitiesCard-1.svg",
+      },
+      {
+        id: 2,
+        name: "Palm Jumeirah",
+        desc: "Ideal island backed with beautiful beaches overlooking stunning views of Skyline.",
+        img: "/assets/img/communitiesCard-2.svg",
+      },
+      {
+        id: 3,
+        name: "Dubai Hill Estate",
+        desc: "The city’s hub, Dubai Marina is a meticulously planned community.",
+        img: "/assets/img/communitiesCard-3.svg",
+      },
+      {
+        id: 4,
+        name: "Dubai Marina",
+        desc: "A lush 2700 acers suburban lifestyle with  lush greenery. ",
+        img: "/assets/img/communitiesCard-4.svg",
+      },
+      {
+        id: 5,
+        name: "Dubai Square",
+        desc: "Town square is divided into 13 sub-projects spread among 1.6 million square feet of land.",
+        img: "/assets/img/communitiesCard-5.svg",
+      },
+      {
+        id: 6,
+        name: "Down Town Dubai",
+        desc: "Downtown Dubai, as the name implies, is the heart of Dubai, constantly evolving.",
+        img: "/assets/img/communitiesCard-6.svg",
+      },
+    ],
+  },
 ];
-
