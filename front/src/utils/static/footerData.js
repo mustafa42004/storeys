20e2c6cabcafc18f7f2f07e9a32b7727a0b6fe0c
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-03-10 07:29:39 +0530

    fix: service page path issues

--- front/src/utils/static/footerData.js
+++ front/src/utils/static/footerData.js
@@ -1,142 +1,141 @@
 export const footerLinks = [
-    {
-        title: "Quick Links",
-        links: [
-            {
-                name: "Home",
-                link: "/"
-            },
-            {
-                name: "About Us",
-                link: "/about"
-            },
-            {
-                name: "Our Team",
-                link: "/team"
-            },
-            {
-                name: "List a Property",
-                link: "/property"
-            },
-            {
-                name: "Careers",
-                link: "/careers"
-            },
-            {
-                name: "Dubai Communities",
-                link: "/communities"
-            },
-            {
-                name: "Complaint",
-                link: "/complaint"
-            },
-            {
-                name: "Dubai News",
-                link: "/news"
-            },
-        ]
-    },
-    {
-        title: "Our Services",
-        links: [
-            {
-                id : 1,
-                name: "Buy Property",
-                desc: "Find your dream home or the perfect investment property with our exclusive listings and market expertise. We guide you through the entire process, from property selection to negotiation and final handover, ensuring a smooth and hassle-free purchase.",
-                link: "/services/buy-property",
-                img:"/front/public/assets/img/service-banner-1.svg"
-            },
-            {
-                id : 2,
-                name: "Sell Property",
-                desc:"Maximize your property's value with our strategic selling approach. From professional marketing to accurate property valuation, we help sellers achieve the best price in the shortest time through our extensive network and cutting-edge marketing techniques.",
-                link: "/services/sell-property",
-                img:"/front/public/assets/img/service-banner-2.svg"
-            },
-            {
-                id : 3,
-                name: "Rent Property",
-                desc: "Whether you're a tenant searching for the perfect home or a landlord looking to lease your property, we provide personalized rental solutions that match your needs. Our team ensures fair pricing, seamless paperwork, and complete peace of mind.",
-                link: "/services/rent-property",
-                img:"/front/public/assets/img/service-banner-3.svg"
-            },
-            {
-                id : 4,
-                name: "Property Management",
-                desc: "Find your dream home or the perfect investment property with our exclusive listings and market expertise. We guide you through the entire process, from property selection to negotiation and final handover, ensuring a smooth and hassle-free purchase.",
-                link: "/services/property-management",
-                img:"/front/public/assets/img/service-banner-4.svg"
-            },
-            {   
-                id : 5,
-                name: "Mortgage",
-                desc:"Navigating the mortgage process can be complex, but our experts simplify it for you. We offer tailored mortgage solutions, helping you secure the best financing options with competitive rates and flexible payment plans.",
-                link: "/services/mortgage",
-                img:"/front/public/assets/img/service-banner-5.svg"
-            },
-            {
-                id : 6,
-                name: "Off-Plan",
-                desc: "Invest in the future with exclusive off-plan properties from top developers. Whether you're looking for high ROI opportunities or a home with cutting-edge designs, we connect you with the best projects before they hit the market.",
-                link: "/services/off-plan",
-                img:"/front/public/assets/img/service-banner-6.svg"
-            },
-            {   
-                id: 7,
-                name: "Holiday Homes",
-                desc:"Turn your property into a profitable short-term rental with our specialized holiday home management services. We handle everything from guest bookings and property upkeep to marketing on leading platforms, ensuring high occupancy rates.",
-                link: "/services/holiday-homes",
-                img:"/front/public/assets/img/service-banner-7.svg"
-            },
-            {
-                id : 8,
-                name: "Commercial",
-                desc: "From office spaces to retail and industrial properties, we assist businesses in finding the ideal commercial spaces to grow and thrive. Whether leasing, buying, or selling, our team offers tailored solutions to match your business needs.",
-                link: "/services/commercial",
-                img:"/front/public/assets/img/service-banner-8.svg"
-            },
-            {  
-                id: 9,
-                name: "Interiors",
-                desc: "Transform your space with Storeys Real Estate's bespoke interior design services. From elegant home designs to commercial spaces, we offer tailored solutions, maximizing comfort, functionality, and style. Increase property value and create lasting impressions with expert design.",
-                link: "/services/interiors",
-                img:"/front/public/assets/img/service-banner-8.svg"
-            },
-
-        ]
-    },
-    {
-        title: "Get in Touch",
-        links: [
-            {
-                name: "056 789 7077"
-            },
-            {
-                name: "enquiries@storeys.ae"
-            },
-            {
-                name: "G10, Building No. 3,"
-            },
-            {
-                name: "Arenco Offices,"
-            },
-            {
-                name: "DIP, Dubai"
-            }
-        ],
-        buttons: [
-            {
-                icon: "/assets/img/instagram.svg",
-                link: "/instagram"
-            },
-            {
-                icon: "/assets/img/facebook.svg",
-                link: "/facebook"
-            },
-            {
-                icon: "/assets/img/twitter.svg",
-                link: "/twitter"
-            },
-        ]
-    }
-]
+  {
+    title: "Quick Links",
+    links: [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: "About Us",
+        link: "/about",
+      },
+      {
+        name: "Our Team",
+        link: "/team",
+      },
+      {
+        name: "List a Property",
+        link: "/property",
+      },
+      {
+        name: "Careers",
+        link: "/careers",
+      },
+      {
+        name: "Dubai Communities",
+        link: "/communities",
+      },
+      {
+        name: "Complaint",
+        link: "/complaint",
+      },
+      {
+        name: "Dubai News",
+        link: "/news",
+      },
+    ],
+  },
+  {
+    title: "Our Services",
+    links: [
+      {
+        id: 1,
+        name: "Buy Property",
+        desc: "Find your dream home or the perfect investment property with our exclusive listings and market expertise. We guide you through the entire process, from property selection to negotiation and final handover, ensuring a smooth and hassle-free purchase.",
+        link: "/services/buy-property",
+        img: "/assets/img/service-banner-1.svg",
+      },
+      {
+        id: 2,
+        name: "Sell Property",
+        desc: "Maximize your property's value with our strategic selling approach. From professional marketing to accurate property valuation, we help sellers achieve the best price in the shortest time through our extensive network and cutting-edge marketing techniques.",
+        link: "/services/sell-property",
+        img: "/assets/img/service-banner-2.svg",
+      },
+      {
+        id: 3,
+        name: "Rent Property",
+        desc: "Whether you're a tenant searching for the perfect home or a landlord looking to lease your property, we provide personalized rental solutions that match your needs. Our team ensures fair pricing, seamless paperwork, and complete peace of mind.",
+        link: "/services/rent-property",
+        img: "/assets/img/service-banner-3.svg",
+      },
+      {
+        id: 4,
+        name: "Property Management",
+        desc: "Find your dream home or the perfect investment property with our exclusive listings and market expertise. We guide you through the entire process, from property selection to negotiation and final handover, ensuring a smooth and hassle-free purchase.",
+        link: "/services/property-management",
+        img: "/assets/img/service-banner-4.svg",
+      },
+      {
+        id: 5,
+        name: "Mortgage",
+        desc: "Navigating the mortgage process can be complex, but our experts simplify it for you. We offer tailored mortgage solutions, helping you secure the best financing options with competitive rates and flexible payment plans.",
+        link: "/services/mortgage",
+        img: "/assets/img/service-banner-5.svg",
+      },
+      {
+        id: 6,
+        name: "Off-Plan",
+        desc: "Invest in the future with exclusive off-plan properties from top developers. Whether you're looking for high ROI opportunities or a home with cutting-edge designs, we connect you with the best projects before they hit the market.",
+        link: "/services/off-plan",
+        img: "/assets/img/service-banner-6.svg",
+      },
+      {
+        id: 7,
+        name: "Holiday Homes",
+        desc: "Turn your property into a profitable short-term rental with our specialized holiday home management services. We handle everything from guest bookings and property upkeep to marketing on leading platforms, ensuring high occupancy rates.",
+        link: "/services/holiday-homes",
+        img: "/assets/img/service-banner-7.svg",
+      },
+      {
+        id: 8,
+        name: "Commercial",
+        desc: "From office spaces to retail and industrial properties, we assist businesses in finding the ideal commercial spaces to grow and thrive. Whether leasing, buying, or selling, our team offers tailored solutions to match your business needs.",
+        link: "/services/commercial",
+        img: "/assets/img/service-banner-8.svg",
+      },
+      {
+        id: 9,
+        name: "Interiors",
+        desc: "Transform your space with Storeys Real Estate's bespoke interior design services. From elegant home designs to commercial spaces, we offer tailored solutions, maximizing comfort, functionality, and style. Increase property value and create lasting impressions with expert design.",
+        link: "/services/interiors",
+        img: "/assets/img/service-banner-8.svg",
+      },
+    ],
+  },
+  {
+    title: "Get in Touch",
+    links: [
+      {
+        name: "056 789 7077",
+      },
+      {
+        name: "enquiries@storeys.ae",
+      },
+      {
+        name: "G10, Building No. 3,",
+      },
+      {
+        name: "Arenco Offices,",
+      },
+      {
+        name: "DIP, Dubai",
+      },
+    ],
+    buttons: [
+      {
+        icon: "/assets/img/instagram.svg",
+        link: "/instagram",
+      },
+      {
+        icon: "/assets/img/facebook.svg",
+        link: "/facebook",
+      },
+      {
+        icon: "/assets/img/twitter.svg",
+        link: "/twitter",
+      },
+    ],
+  },
+];
