a487978ee27941fb0858620051d6883591f662dd
Abbas Bhanpura wala <abbasbhanpura.dev@gmail.com>
2025-03-10 20:55:29 +0530

    fix: shadow issue

--- front/src/components/feature/Landing/Developers/DevSection.jsx
+++ front/src/components/feature/Landing/Developers/DevSection.jsx
@@ -1,24 +1,25 @@
-import { useState, useEffect } from "react"
-import Heading from "../../../shared/Headings/Heading"
-import Slider from "./Slider"
+import { useState, useEffect } from "react";
+import Heading from "../../../shared/Headings/Heading";
+import Slider from "./Slider";
 
 const DevSection = () => {
-  const [isMobile , setIsMobile] = useState(false)
-  
-  useEffect(()=>{
-    window.innerWidth > 767 ? setIsMobile(false) : setIsMobile(true)
-  },[])
-  
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    window.innerWidth > 767 ? setIsMobile(false) : setIsMobile(true);
+  }, []);
 
   return (
     <>
-        <section className="dev-section pt-cs">
-            <Heading title="Our Developers" className={ `${isMobile ? "fs-50 w-80 " : "auto"}`}/>
-            <Slider />
-        </section>
+      <section className="dev-section pt-cs">
+        <Heading
+          title="Our Developers"
+          className={`${isMobile ? "fs-50 w-80 " : "auto"}`}
+        />
+        <Slider />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default DevSection
+export default DevSection;
