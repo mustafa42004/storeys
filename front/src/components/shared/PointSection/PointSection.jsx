dc1d61dce4f6e614b56afa948735f4efa79a2546
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2025-03-21 19:45:55 +0530

    fix: vision section responsive issue

--- front/src/components/shared/PointSection/PointSection.jsx
+++ front/src/components/shared/PointSection/PointSection.jsx
@@ -1,20 +1,22 @@
 const PointSection = ({ data }) => {
   return (
     <>
-        <div className="point-section">
-            {
-                data?.map((item, index) => (
-                    <div className="item" key={index}>
-                        <h4 className="font-lg font-sans bold fs-70">
-                            {Number(index) + 1 < 10 ? `0${Number(index) + 1}` : Number(index) + 1}
-                        </h4>
-                        <p className="font-sm medium text-left fs-22"><span className="fw-600">{item.header} -</span> {item.description}</p>
-                    </div>
-                ))
-            }
-        </div>
+      <div className="point-section">
+        {data?.map((item, index) => (
+          <div className="item" key={index}>
+            <h4 className="font-lg font-sans bold fs-70">
+              {Number(index) + 1 < 10
+                ? `0${Number(index) + 1}`
+                : Number(index) + 1}
+            </h4>
+            <p className="font-sm medium text-left fs-22">
+              <span className="fw-600">{item.header} -</span> {item.description}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PointSection
+export default PointSection;
